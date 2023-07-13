e6a3a9fba910e5e9ebcc4e4652af97f961c666de
Jose Julian <jose.julianm2505@gmail.com>
2023-07-12 20:41:41 -0400

    test eliminated

--- src/test/java/com/rd/CarsharingApplicationTests.java
+++ src/test/java/com/rd/CarsharingApplicationTests.java
@@ -2,22 +2,10 @@ package com.rd;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.scheduling.annotation.Async;
 
 @SpringBootTest
 class CarsharingApplicationTests {
 
-    @Test
-    void contextLoads() {
-        String password = "123456789";
-        String password2 = "123456e78";
-        String password3 = "12345678E78";
-        String password4 = "#Hola2525";
-        String regex = "^(?=.*[a-zA-Z])(?=.*\\d)[a-zA-Z0-9!@#$%^&*()\\-_=+{}\\[\\]|;:'\",.<>/?]{8,16}$";
-        System.out.println(password.matches(regex));
-        System.out.println(password2.matches(regex));
-        System.out.println(password3.matches(regex));
-        System.out.println(password4.matches(regex));
-
-    }
 
 }
