3eebf17635d4c0b5c13d94554eb7069806502cbf
Jose Julian <jose.julianm2505@gmail.com>
2023-08-01 18:23:55 -0400

    conditional deleted

--- src/main/java/com/rd/jwt/JwtService.java
+++ src/main/java/com/rd/jwt/JwtService.java
@@ -7,7 +7,6 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
-
 import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
@@ -44,7 +43,7 @@ public class JwtService {
 
     public boolean isTokenValid(String token, UserDetails userDetails) {
         final String username = extractUsername(token);
-        return(username.equals(userDetails.getUsername())) && !isTokenExpired(token) && userDetails.isEnabled();
+        return(username.equals(userDetails.getUsername())) && !isTokenExpired(token);
     }
 
     private boolean isTokenExpired(String token) {
