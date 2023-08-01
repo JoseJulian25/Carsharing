19042ef04956d8158175f24c8cd6d842bbb9e8d0
Jose Julian <jose.julianm2505@gmail.com>
2023-08-01 19:07:39 -0400

    fix: error to create a payment

--- src/main/java/com/rd/DTO/request/PaymentRequestDTO.java
+++ src/main/java/com/rd/DTO/request/PaymentRequestDTO.java
@@ -1,10 +1,20 @@
 package com.rd.DTO.request;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Builder
-@Data
+@Setter
+@Getter
 public class PaymentRequestDTO {
     private String paymentMethod;
+
+    @JsonCreator
+    public PaymentRequestDTO(@JsonProperty("paymentMethod") String paymentMethod){
+        this.paymentMethod = paymentMethod;
+    }
 }
